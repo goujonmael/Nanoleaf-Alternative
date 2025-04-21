b1d7c3fb8f8f0bf75d96387aefc3e1da6101fa8e
Goujon Maël <goujonmael@gmail.com>
2025-04-21 21:52:03 +0200

    Update RGB Led Strip.ino

--- Nanoleaf/RGB Led Strip.ino
+++ Nanoleaf/RGB Led Strip.ino
@@ -1,149 +1,93 @@
-// Uncomment the following line to enable serial debug output
-//#define ENABLE_DEBUG
+#include <ESP8266WiFi.h>
+#include <ESP8266WebServer.h>
 
-#ifdef ENABLE_DEBUG
-       #define DEBUG_ESP_PORT Serial
-       #define NODEBUG_WEBSOCKETS
-       #define NDEBUG
-#endif 
+// Remplacez par vos identifiants WiFi
+const char* ssid = "MYSSID";
+const char* password = "MYPASS";
+// Pins pour les LEDs
+const int bluePin = 14; // D5
+const int redPin = 13; // D6
+const int greenPin = 12; // D7
 
-#include <Arduino.h>
-#ifdef ESP8266 
-       #include <ESP8266WiFi.h>
-#endif 
-#ifdef ESP32   
-       #include <WiFi.h>
-#endif
+ESP8266WebServer server(80);
+ESP8266WebServer serverSecure(443);
 
-#include "SinricPro.h"
-#include "SinricProLight.h"
-
-#define WIFI_SSID         "Miguel"    
-#define WIFI_PASS         "Banane26"
-#define APP_KEY           "85f5bb93-8161-43e5-afcf-cf817fbac696"             // Should look like "de0bxxxx-1x3x-4x3x-ax2x-5dabxxxxxxxx"
-#define APP_SECRET        "227078b3-3960-4099-820e-1ca86c04888e-d57f6ebc-b96a-4cc1-b58d-c84c6bad550d"          // Should look like "5f36xxxx-x3x7-4x3x-xexe-e86724a9xxxx-4c4axxxx-3x3x-x5xe-x9x3-333d65xxxxxx"
-#define LIGHT_ID          "600c68d5905d812f2410e4b2"           // Should look like "5dc1564130xxxxxxxxxxxxxx"
-#define BAUD_RATE         9600                      // Change baudrate to your need for serial log
-
-#define BLUE_PIN          14                        // PIN for BLUE Mosfet  - change this to your need (D5 = GPIO14 on ESP8266)
-#define RED_PIN           13                        // PIN for RED Mosfet   - change this to your need (D6 = GPIO12 on ESP8266)
-#define GREEN_PIN         12                        // PIN for GREEN Mosfet - change this to your need (D7 = GPIO13 on ESP8266)
-#define FADESPEED 5
-
-struct {                                            // Stores current device state with following initial values:
-  bool powerState = false;                          // initial state is off
-  int brightness = 100;                             // initial brightness is set to 100
-  struct {
-    byte r = 255;                                   // color
-    byte g = 255;                                   // is set 
-    byte b = 255;                                   // to white
-  } color;
-} device_state; 
-
-// setStripe: sets the mosfets values corresponding to values stored in device_state
-void setStripe() {
-  int rValue = map(device_state.color.r * device_state.brightness, 0, 255 * 100, 0, 1023);  // calculate red value and map between 0 and 1023 for analogWrite
-  int gValue = map(device_state.color.g * device_state.brightness, 0, 255 * 100, 0, 1023);  // calculate green value and map between 0 and 1023 for analogWrite
-  int bValue = map(device_state.color.b * device_state.brightness, 0, 255 * 100, 0, 1023);  // calculate blue value and map between 0 and 1023 for analogWrite
-
-  if (device_state.powerState == false) {           // turn off?
-    digitalWrite(RED_PIN,   LOW);                   // set
-    digitalWrite(GREEN_PIN, LOW);                   // mosfets
-    digitalWrite(BLUE_PIN,  LOW);                   // low
-  } 
-  else {
-    if (device_state.color.r==255 && device_state.color.g==191 && device_state.color.b==204){
-      fade();
-    }
-    else {
-    analogWrite(RED_PIN,   rValue);                 // write red value to pin
-    analogWrite(GREEN_PIN, gValue);                 // write green value to pin
-    analogWrite(BLUE_PIN,  bValue);                 // write blue value to pin
-  }}
+void handleRoot() {
+  String html = "<!DOCTYPE html><html><head><title>Contrôle des LEDs</title></head><body>";
+  html += "<h1>Contrôle des LEDs</h1>";
+  html += "<form action='/setColor' method='GET'>";
+  html += "Rouge: <input type='range' name='r' min='0' max='1023'><br>";
+  html += "Vert: <input type='range' name='g' min='0' max='1023'><br>";
+  html += "Bleu: <input type='range' name='b' min='0' max='1023'><br>";
+  html += "<input type='submit' value='Envoyer'>";
+  html += "</form></body></html>";
+  server.send(200, "text/html", html);
+  Serial.println("Page principale servie");
 }
 
-bool onPowerState(const String &deviceId, bool &state) {
-  device_state.powerState = state;                  // store the new power state
-  setStripe();                                      // update the mosfets
-  return true;
+void handleSetColor() {
+  if (server.hasArg("r") && server.hasArg("g") && server.hasArg("b")) {
+    int r = server.arg("r").toInt();
+    int g = server.arg("g").toInt();
+    int b = server.arg("b").toInt();
+
+    analogWrite(redPin, r);
+    analogWrite(greenPin, g);
+    analogWrite(bluePin, b);
+
+    Serial.print("Couleurs mises à jour : R=");
+    Serial.print(r);
+    Serial.print(", G=");
+    Serial.print(g);
+    Serial.print(", B=");
+    Serial.println(b);
+  }
+  server.send(200, "text/plain", "Couleurs mises à jour");
 }
 
-bool onBrightness(const String &deviceId, int &brightness) {
-  device_state.brightness = brightness;             // store new brightness level
-  setStripe();                                      // update the mosfets
-  return true;
-}
-
-bool onAdjustBrightness(const String &deviceId, int &brightnessDelta) {
-  device_state.brightness += brightnessDelta;       // calculate and store new absolute brightness
-  brightnessDelta = device_state.brightness;        // return absolute brightness
-  setStripe();                                      // update the mosfets
-  return true;
-}
-
-bool onColor(const String &deviceId, byte &r, byte &g, byte &b) {
-  device_state.color.r = r;                         // store new red value
-  device_state.color.g = g;                         // store new green value
-  device_state.color.b = b;                         // store new blue value
-  setStripe();                                      // update the mosfets
-  return true;
+void handleRedirect() {
+  Serial.println("Redirection de HTTPS vers HTTP");
+  serverSecure.sendHeader("Location", "http://" + WiFi.localIP().toString(), true);
+  serverSecure.send(302, "text/plain", ""); // Redirection HTTP 302
+  serverSecure.client().stop(); // Fermer la connexion client
 }
 
 void setup() {
-  pinMode(RED_PIN,   OUTPUT);                       // set red-mosfet pin as output
-  pinMode(GREEN_PIN, OUTPUT);                       // set green-mosfet pin as output
-  pinMode(BLUE_PIN,  OUTPUT);                       // set blue-mosfet pin as output
+  Serial.begin(115200);
+  Serial.println("Démarrage...");
 
-  Serial.begin(BAUD_RATE);                          // setup serial 
-
-  WiFi.begin(WIFI_SSID, WIFI_PASS);                 // connect wifi
-
-  SinricProLight &myLight = SinricPro[LIGHT_ID];    // create light device
-  myLight.onPowerState(onPowerState);               // set PowerState callback
-  myLight.onBrightness(onBrightness);               // set Brightness callback
-  myLight.onAdjustBrightness(onAdjustBrightness);   // set AdjustBrightness callback
-  myLight.onColor(onColor);                         // set Color callback
-
-  SinricPro.begin(APP_KEY, APP_SECRET);             // start SinricPro 
-}
-
-void fade(){
-  for (int i = 0; i < 10; i = i + 1) {
-  int r, g, b;
- 
-  // fade from blue to violet
-  for (r = 0; r < 256; r++) { 
-    analogWrite(RED_PIN, r);
-    delay(FADESPEED);
-  } 
-  // fade from violet to red
-  for (b = 255; b > 0; b--) { 
-    analogWrite(BLUE_PIN, b);
-    delay(FADESPEED);
-  } 
-  // fade from red to yellow
-  for (g = 0; g < 256; g++) { 
-    analogWrite(GREEN_PIN, g);
-    delay(FADESPEED);
-  } 
-  // fade from yellow to green
-  for (r = 255; r > 0; r--) { 
-    analogWrite(RED_PIN, r);
-    delay(FADESPEED);
-  } 
-  // fade from green to teal
-  for (b = 0; b < 256; b++) { 
-    analogWrite(BLUE_PIN, b);
-    delay(FADESPEED);
-  } 
-  // fade from teal to blue
-  for (g = 255; g > 0; g--) { 
-    analogWrite(GREEN_PIN, g);
-    delay(FADESPEED);
+  // Connexion au WiFi
+  WiFi.begin(ssid, password);
+  Serial.print("Connexion au WiFi ");
+  Serial.print(ssid);
+  Serial.println("...");
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.print(".");
   }
-  } 
+  Serial.println("");
+  Serial.println("Connecté au WiFi");
+  Serial.print("Adresse IP : ");
+  Serial.println(WiFi.localIP());
+
+  // Configuration des pins
+  pinMode(redPin, OUTPUT);
+  pinMode(greenPin, OUTPUT);
+  pinMode(bluePin, OUTPUT);
+
+  // Configuration du serveur web sur le port 80
+  server.on("/", handleRoot);
+  server.on("/setColor", handleSetColor);
+  server.begin();
+  Serial.println("Serveur web démarré sur le port 80");
+
+  // Configuration du serveur web sur le port 443 pour la redirection
+  serverSecure.onNotFound(handleRedirect);
+  serverSecure.begin();
+  Serial.println("Serveur web démarré sur le port 443 pour la redirection");
 }
 
 void loop() {
-  SinricPro.handle();                               // handle SinricPro communication
+  server.handleClient();
+  serverSecure.handleClient();
 }
